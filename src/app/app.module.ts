742c8edc3030cb9b8682aed13830b9f4dbcb547b
Daniel Gross <danwgross@gmail.com>
2019-07-29 17:33:32 -0400

    added components for auth views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,18 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IosInstallComponent } from './ios-install/ios-install.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoaderComponent } from './loader/loader.component';
+import { CountryCodeSelectComponent } from './auth/country-code-select/country-code-select.component';
+import { FilterPipe } from './auth/country-code-select/filter.pipe';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { ConfirmCodeComponent } from './auth/confirm-code/confirm-code.component';
+import { ProfileComponent } from './auth/profile/profile.component';
+import { AvatarComponent } from './auth/profile/avatar/avatar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, IosInstallComponent],
+  declarations: [AppComponent, HomeComponent, IosInstallComponent, AuthComponent, LoaderComponent, CountryCodeSelectComponent, FilterPipe, SignInComponent, SignUpComponent, ConfirmCodeComponent, ProfileComponent, AvatarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,6 +33,6 @@ import { IosInstallComponent } from './ios-install/ios-install.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [IosInstallComponent],
+  entryComponents: [IosInstallComponent, LoaderComponent, CountryCodeSelectComponent],
 })
 export class AppModule {}
---
+++ src/app/auth/country-code-select/country-code-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-code-select',
+  templateUrl: './country-code-select.component.html',
+  styleUrls: ['./country-code-select.component.scss']
+})
+export class CountryCodeSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
